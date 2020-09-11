67cabdb7642a7eef520625379e76de812e7a4c1e
gmorehead <gmorehead@vionxenergy.com>
2020-09-11 14:49:37 -0400

    Standalone simpleui-server is compiling.

---
+++ simpleui-server/post-build-step.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+PROJ=simpleui-server
+
+echo "------------------ Starting post-build-step -----------------------"
+
+
+if $(! test -d dist/simpleui-server/node_modules); then
+    mkdir dist/simpleui-server/node_modules
+fi
+
+for d in $(cat src/node_modules-dirs-to-copy.txt); do
+    if $(! test -d dist/simpleui-server/node_modules/${d}); then
+        cp -r ${d} dist/simpleui-server/node_modules;
+    fi
+done
+
+# Create dist.tgz
+cd dist
+tar czvf ../dist.tgz --exclude="${PROJ}"  *
+cd ..
+
+echo "------------------ Finished post-build-step at $(date) -----------------------"
