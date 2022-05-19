59e11b0aea91c480278dda2d423aa991b31a1b18
jscarsdale <jscarsdale@largocleanenergy.com>
2022-05-19 17:08:28 -0400

    WHen run from the IDE, the base_app and simpleui-server builds should create distribution files that can be overwritten with the next build.

--- base_app/post-build-step.bash
+++ base_app/post-build-step.bash
@@ -72,6 +72,7 @@ if [[ "${NPM_COPY_DIST_TO_OUTPUT}" != "false" ]]; then
 
     echo Copy files to ${out_folder} folder;
     cp dist.tgz  ${out_folder}
+    chmod 755 ${out_folder}/dist.tgz
 fi
 
 echo "------------------ Finished post-build-step at $(date) -----------------------"
