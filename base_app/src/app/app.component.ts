9212dee9ae87248b9000d671b0aa01313ceac669
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2022-11-01 08:44:24 -0400

    enable page refresh for gc

--- base_app/src/app/app.component.ts
+++ base_app/src/app/app.component.ts
@@ -617,10 +617,9 @@ export class AppComponent implements OnInit, AfterViewInit /*, OnChanges */ {
         try {
             if ((cycle * this._refreshRate) > this._milliSecondsBeforeAutoPageReload) {
                 sessionStorage.autoReload = 'true';
-                //setTimeout(() => {
-                    //document.location.reload();
-                    //console.log('----------------------------------------- attempted reload...')
-                //}, 1);
+                setTimeout(() => {
+                    document.location.reload();
+                }, 1);
             }
 
             if (!AppComponent._updatesSuspended) {
