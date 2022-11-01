b733b5188181d7ebe553166d9d9ac83890568cd6
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2022-11-01 13:33:58 -0400

    Made zmq backend more robust

--- docker_zmq/backend.py
+++ docker_zmq/backend.py
@@ -3,7 +3,6 @@ A ZMQ backend to interact with simpleui-server
 Zack Beucler 10/28/22
 """
 
-import time
 import zmq
 
 
@@ -16,22 +15,35 @@ def main():
     context = zmq.Context()
     data_socket = context.socket(zmq.REP)
     data_socket.bind('tcp://*:1234')
+    data_msg = "no data recieved..."
 
     cmd_socket = context.socket(zmq.REP)
     cmd_socket.bind('tcp://*:5482')
-    print('starting server...')
+    cmd_msg = "no cmd recieved..."
 
+
+    print('starting server...')
     while True:
         try:
-            msg = data_socket.recv()
+            data_msg = data_socket.recv()
         except zmq.ZMQError as e:
             if e.errno == zmq.ETERM:
                 print(e)
             else:
                 raise
-        print(msg)
- 
-
+        print(data_msg)
         data_socket.send(table_data)
+
+
+        try:
+            cmd_msg = cmd_socket.recv()
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                print(e)
+            else:
+                raise
+        print(cmd_msg)
+        #cmd_socket.send("command?")
+
 
 main()
