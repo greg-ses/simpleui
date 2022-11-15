d575e9302ca69cf933026b3690903075b357b9e9
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2022-11-15 16:34:58 -0500

    added a limit of 32 items to the queue

--- simpleui-server/src/queue.ts
+++ simpleui-server/src/queue.ts
@@ -2,9 +2,11 @@
 
 export class Queue {
     elements: Array<any>;
+    MAX_QUEUE_SIZE: number;
 
     constructor() {
         this.elements = [];
+        this.MAX_QUEUE_SIZE = 32;
     }
 
     /**
@@ -12,7 +14,11 @@ export class Queue {
      * @param element 
      */
     enqueue(element: any) {
-        this.elements.push(element)
+        while (this.elements.length >= this.MAX_QUEUE_SIZE) {
+            console.log(this.elements.length)
+            this.dequeue();
+        }
+        this.elements.push(element);
     }
 
     /**
@@ -33,6 +39,10 @@ export class Queue {
 
     get length() {
         return this.elements.length
+    }
+
+    set max_queue_length(val: number) {
+        this.MAX_QUEUE_SIZE = val;
     }
 
 }
