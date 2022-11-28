f8bcd2247eb66fc8f64bb023f553b448fbaecb26
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2022-11-28 09:11:35 -0500

    added tini to dev env for quick shutdown

--- develop_docker/install-script.bash
+++ develop_docker/install-script.bash
@@ -11,14 +11,6 @@
     fi
 )
 
-# -------------- base_app --------------
-cd /usr/src/app/base_app || exit 1
-if [[ ! -d node_modules ]]; then
-    npm install;
-fi
-#npm run build-client-dev;
-
-ng serve --host 0.0.0.0 --port 4200& #--configuration development
 
 
 # ----------- simpleui-server -----------
@@ -28,9 +20,21 @@ if [[ ! -d node_modules ]]; then
 fi
 npm run build;
 
-node /usr/src/app/simpleui-server/dist/simpleui-server/simpleui-server.js \
+/tini -- node /usr/src/app/simpleui-server/dist/simpleui-server/simpleui-server.js \
 --mode=daemon \
 --appName=simple_ui \
---webPort=4100
+--webPort=4100 &
+
+
+# -------------- base_app --------------
+cd /usr/src/app/base_app || exit 1
+if [[ ! -d node_modules ]]; then
+    npm install;
+fi
+#npm run build-client-dev;
+
+ng serve --host 0.0.0.0 --port 4200 #--configuration development
+
+
 
 
