0f892ea83b3a9d62805acdb7d41d8928f1402643
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2023-01-26 17:04:31 -0500

    fixed prop-defined-table test and now it passes

--- base_app/src/app/prop-defined-table/prop-defined-table.spec.ts
+++ base_app/src/app/prop-defined-table/prop-defined-table.spec.ts
@@ -2,20 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PropDefinedTableComponent } from './prop-defined-table';
 
+import { AppComponent } from '../app.component';
+
+
 describe('PropDefinedTableComponent', () => {
   let component: PropDefinedTableComponent;
   let fixture: ComponentFixture<PropDefinedTableComponent>;
+  let app: AppComponent;
+
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ PropDefinedTableComponent ]
     })
     .compileComponents();
+
+    app = new AppComponent();
+
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PropDefinedTableComponent);
     component = fixture.componentInstance;
+    component.app = app;
     fixture.detectChanges();
   });
 
