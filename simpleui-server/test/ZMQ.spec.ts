e13d3391dac153c103b4b3e1a7a6238e241b501a
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2023-02-09 12:57:15 -0500

    Integrated jest and made ZMQ tests

---
+++ simpleui-server/test/ZMQ.spec.ts
@@ -0,0 +1,100 @@
+var _zmq = require('zeromq');
+import { ZMQ_Socket_Wrapper } from '../src/sui_zmq';
+import { Logger, LogLevel } from '../src/server-logger';
+
+// setup logger
+Logger.setLogLevel("VERBOSE");
+
+
+class ZMQ_Reply_Socket {
+    socket: any
+    port: number;
+    address: string;
+
+    constructor(port: number) {
+        try {
+            this.port = port;
+            this.address = `tcp://127.0.0.1:${this.port}`;
+            this.socket = _zmq.socket('rep');
+            this.socket.setsockopt(_zmq.ZMQ_LINGER, 0);
+            this.socket.bindSync(this.address);
+        } catch (err) {
+            Logger.log(LogLevel.ERROR, `Could not make REPLY socket on ${this.address}, got error: ${err}`);
+        }
+    }
+    send(msg: string) {
+        try {
+            this.socket.send(msg);
+        } catch (err) {
+            Logger.log(LogLevel.ERROR, `Could not send zmq message: ${msg} got error: ${err}`);
+        }
+    }
+    close() {
+        try {
+            if (this.socket.closed === false) {
+                this.socket.close();
+            }
+        } catch (err) {
+            Logger.log(LogLevel.ERROR, `Could not close socket ${this.port}, got error: ${err}`);
+        }
+    }
+}
+
+
+
+
+
+const PORT = 1234;
+const HOSTNAME = '127.0.0.1';
+let mock_reply_socket: ZMQ_Reply_Socket;
+let test_request_socket: ZMQ_Socket_Wrapper;
+
+
+describe('ZMQ_Socket_Wrapper Class testing', () => {
+    beforeEach( () => {
+        mock_reply_socket = new ZMQ_Reply_Socket(PORT);
+        test_request_socket = new ZMQ_Socket_Wrapper(HOSTNAME, PORT);
+    });
+    afterEach( () => {
+        mock_reply_socket.close();
+        test_request_socket.close();
+    });
+
+
+
+    test('Class instance exist', () => {
+        expect(test_request_socket).toBeTruthy();
+    });
+
+
+
+    // also tests that sockets connect on init (bc of beforeEach())
+    test('REQ socket should recieve message from REP socket', done => {
+        const test_message = "ABCDEFG";
+        test_request_socket.send(test_message);
+        mock_reply_socket.socket.on('message', (raw_data: any) => {
+            const zmq_msg = raw_data.toString();
+            expect(zmq_msg).toEqual(test_message);
+            done();
+        });
+    });
+
+
+
+    test('REQ socket sends message when item is added to its queue', done => {
+        let test_message = "ABCDEFG";
+        mock_reply_socket.socket.on('message', (raw_data: any) => {
+            const zmq_msg = raw_data.toString();
+            expect(zmq_msg).toEqual(`<request COMMAND="${test_message}" valueName=""/>`);
+            done();
+        });
+
+        let mock_req = {
+            query: { cmd: test_message },
+            params: { zmqValue: "" }
+        }
+        test_request_socket.http_queue.enqueue([{}, mock_req]);
+    }, 5_000);
+
+});
+
