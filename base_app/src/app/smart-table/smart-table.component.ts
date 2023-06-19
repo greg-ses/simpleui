a13bf7f72c2ea8daff1ca15ed5b6737d766ae167
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2023-06-19 12:30:31 -0400

    made smart-table generic

--- base_app/src/app/smart-table/smart-table.component.ts
+++ base_app/src/app/smart-table/smart-table.component.ts
@@ -1,26 +1,15 @@
 import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
-import { TabUI } from '../interfaces/props-data';
-
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-
-export enum Fault_Status {
-  EMPTY = "",
-  NONE = "none",
-  DISABLED = "disabled",
-  WARNING = "warning",
-  TRIP = "trip"
-}
-
 /**
  * TODO
- *    - [ ] double check work I did today and make sure I understand it
- *    - [ ] make faultlist not hardcoded
+ *    - [x] double check work I did today and make sure I understand it
+ *    - [x] make faultlist not hardcoded
  *    - [ ] make sure generic smart-table works with other tables too
  *    - [ ] restrict overlay mod filter to 1 at a time (Mod1 or Mod2, never both)
  *    - [ ] remove filter pipe
- *    - [ ] check if I even need this.current_filter
+ *    - [x] check if I even need this.current_filter
  */
 
 
@@ -29,7 +18,6 @@ export interface Filter_Options {
   possible_options: string[];
   title: string;
   filter_column: string;
-  default?: string|string[];
   allow_multiple_filters?: boolean;
 }
 
@@ -49,48 +37,25 @@ export interface SmartTableConfig {
   styleUrls: ['./smart-table.component.css']
 })
 export class SmartTableComponent implements OnInit, AfterViewInit {
-  @Input() _dataset: any;
-  @Input() _uiTab: TabUI;
-  @Input() _id: string;
+
   @Input() _config: SmartTableConfig;
 
+
   @ViewChild(MatSort) sort: MatSort;
-
-
-  columns = [];
-  filterFaultValue: Fault_Status|Fault_Status[] = Fault_Status.EMPTY;
-  allStatuses = ['none', 'disabled', 'warning', 'trip'];
   datasource: MatTableDataSource<any> =  new MatTableDataSource();
-
-  // curent values we want in the table
-  current_filter: string|string[];
-
-
-
-
 
 
   constructor() { }
 
-  ngOnInit(): void {
-     // set default filters if applicable
-
-    if (this._uiTab?.name.toLowerCase() == 'fault list') {
-      this.filterFaultValue = [Fault_Status.TRIP, Fault_Status.WARNING];
-    }
-
-    /**
-     * remove once faultlist is uncoupled
-     */
-    if (this?._config?.filter_options?.default) {
-      this.current_filter = this._config.filter_options.default;
-    }
-  }
+  ngOnInit(): void {}
 
 
 
   ngAfterViewInit() {
-    if (this?._config.rows.length > 0) {
+    /**
+     * ugly, refactor
+     */
+    if (this?._config?.rows?.length > 0) {
 
       this.datasource = new MatTableDataSource(this?._config.rows);
 
@@ -105,29 +70,17 @@ export class SmartTableComponent implements OnInit, AfterViewInit {
         }
         return false;
       }
-
-
-    } else {
-      this.datasource = new MatTableDataSource(this._dataset?.elements);
-      this.datasource.sort = this.sort;
     }
 
   }
 
 
-
+  /**
+   * Returns column names of table
+   */
   getColumns() {
     if (this?._config?.columns) { return this._config.columns; }
-
-    /**
-     * remove once faultlist is uncoupled
-     */
-    const originalColumns = this._dataset?.props?.columns !== undefined ? this._dataset.props.columns : [];
-    let columns = [];
-    originalColumns.forEach(element => {
-      columns.push(element[0]);
-    });
-    return columns
+    return [];
   }
 
 
@@ -139,20 +92,4 @@ export class SmartTableComponent implements OnInit, AfterViewInit {
   applyFilter(data: any): void {
     this.datasource.filter = data;
   }
-
-
-
-
-
-  /**
-   * remove once faultlist is uncoupled
-   */
-  handleSelection(selectedValues: any) {
-    this.filterFaultValue = selectedValues;
-
-    this.current_filter = selectedValues;
-  }
-
-
-
 }
